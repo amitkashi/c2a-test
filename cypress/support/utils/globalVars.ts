e86142c7d3071df94d62fd327d975bdfc755c24b
amitkashi <amitkashi47@gmail.com>
2023-10-04 17:21:09 +0300

    change script for running cypress

--- cypress/support/utils/globalVars.ts
+++ cypress/support/utils/globalVars.ts
@@ -1,10 +1,3 @@
-// export const user = {
-//   userName: Cypress.env("USER_NAME"),
-//   password: Cypress.env("PASSWORD"),
-// };
-
-// export const chosenPhone = {};
-
 export interface User {
   userName: string;
   password: string;
@@ -22,5 +15,5 @@ export interface ChosenPhone {
 
 export const chosenPhone: ChosenPhone = {
   title: "",
-  price: 0
+  price: 0,
 };
